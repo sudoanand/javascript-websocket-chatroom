462b517e45d296a16c05567af35275577c4f6d5d
Anand Singh] <singh.ak700@gmail.com>
2020-05-18 21:22:34 +0530

    Add websocket

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function sendMessage(e){
-    alert("Hi");
+    var msg = document.getElementById("messageInput").value;
 
+    appendChat(msg);
     return false;
 }
+
 
 function appendChat(message,alignLeft){
     var messageBox;
     
+    //Append m,essage in UI
     if(alignLeft){
         messageBox = `<li class="chatroom_history_list_item chatroom_history_list_left chatroom_history_list_item--grey">${message}</li>`;
     }else{
@@ -16,4 +19,39 @@ function appendChat(message,alignLeft){
     chatHistory.insertAdjacentHTML('beforeend',messageBox);
     chatHistory.scrollTop = chatHistory.scrollHeight;
 
+    //Clear chat input
+    document.getElementById("messageInput").value = '';
+    
+
+    //Send message
+    socket.send(JSON.stringify({
+      "type":"message",
+      "data": message
+    }));
 }
+
+//Websocket setup
+var websocketApiKey = 'oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm';
+var socket = new WebSocket("wss://connect.websocket.in/v3/1?apiKey="+websocketApiKey);
+
+socket.onopen = function(e) {
+    console.log("[open] Connection established");
+  };
+  
+  socket.onmessage = function(event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+  };
+  
+  socket.onclose = function(event) {
+    if (event.wasClean) {
+      console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      console.log('[close] Connection died');
+    }
+  };
+  
+  socket.onerror = function(error) {
+    console.log(`[error] ${error.message}`);
+  };
